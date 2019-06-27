5ce4ad5a1c6ff2d8984a0072476d569b434e797e
DannyRoberts95 <danielrdhr@gmail.com>
2019-06-27 15:45:44 +0100

    Updates

---
+++ precache-manifest.7ba1d4e60316d9c7ada48071620a2f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f46b2581366ba4a42c",
+    "url": "/javawax/static/css/main.67530d17.chunk.css"
+  },
+  {
+    "revision": "a9f46b2581366ba4a42c",
+    "url": "/javawax/static/js/main.a9f46b25.chunk.js"
+  },
+  {
+    "revision": "605fe1b35a61b9e1a2d6",
+    "url": "/javawax/static/css/1.cb762c66.chunk.css"
+  },
+  {
+    "revision": "605fe1b35a61b9e1a2d6",
+    "url": "/javawax/static/js/1.605fe1b3.chunk.js"
+  },
+  {
+    "revision": "f69fbc2003312fe3297c",
+    "url": "/javawax/static/js/runtime~main.f69fbc20.js"
+  },
+  {
+    "revision": "c4819bf6f89dc6e9abbf706882463d85",
+    "url": "/javawax/index.html"
+  }
+];
